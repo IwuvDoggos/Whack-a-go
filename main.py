c35daa2df31165cef19d0309c23fd49861507697
CNunley000 <89652512+CNunley000@users.noreply.github.com>
2022-03-29 14:33:07 -0600

    def place START

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# Conner Nunley
+# Comp Prog 2
+# No Cheating
+
 import tkinter as tk
 
 window = tk.Tk()
 window.title("goban")
 
+# This will be a tsume go program that gives you predetermined tsume go problems and you must continue in the right order
 
 # create a photoimage variable
 black = tk.PhotoImage(file='goBLACK.png')
@@ -14,5 +19,20 @@ empty = empty.zoom(2)
 
 button = tk.Button(image=black)
 button.grid(column=0,row=0)
+
+def place(bStarts,wStarts,bMoves,wMoves,size=19):
+    """This is given starting positions and outputs a board"""
+    for x in range(size):
+        for y in range(size):
+            if (x,y) in bStarts:
+                black_stone = tk.Button(image=black)
+                black_stone.grid(column=x,row=y)
+            elif (x,y) in wStarts:
+                white_stone = tk.Button(image=white)
+                white_stone.grid(column=x,row=y)
+            else:
+                point = tk.Button(image=empty)
+                point.grid(column=x,row=y)
+
 
 tk.mainloop()
